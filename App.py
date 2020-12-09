ebfb73c4bd90d3c74f9b078290c366a4129d432f
obonobo <ethanbnb@gmail.com>
2020-12-09 01:10:02 -0500

    Refactored the selenium stuff into Scraper class

--- App.py
+++ App.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.8
 
 from typing import List
+from bs4 import element
 import requests
 from bs4 import BeautifulSoup
 from bs4.element import ResultSet
@@ -13,8 +14,8 @@ from core.scraper import BUNDLE_URL, TEST_URL, Scraper
 
 
 def main():
-    scraper = Scraper(TEST_URL, search_element=Scraper.BOOK_TITLE_SEARCH)
-    for line in scraper.scrape():
+    scraper = Scraper(TEST_URL, search_element=Scraper._BOOK_TITLE_SEARCH)
+    for line in scraper.scrape_book():
         print(line)
 
     print("============================")
@@ -40,22 +41,23 @@ def main():
         props
     )
 
-    for line in scraper.scrape():
+    for line in scraper.scrape_book():
         print(line)
 
 
 def main2():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.binary_location = "/usr/bin/google-chrome"
-    URL = "https://www.humblebundle.com/"
-    driver = webdriver.Chrome(executable_path="lib/chromedriver",
-                              options=chrome_options)
-    driver.get(URL)
-    driver.find_element_by_class_name("js-bundle-dropdown").click()
-    div: WebElement = driver.find_element_by_class_name("bundle-dropdown-content-wrapper")
-    elements: List[WebElement] = div.find_elements_by_css_selector("span.name")
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--window-size=1920,1080")
+    # chrome_options.binary_location = "/usr/bin/google-chrome"
+    # URL = "https://www.humblebundle.com/"
+    # driver = webdriver.Chrome(executable_path="lib/chromedriver",
+    #                           options=chrome_options)
+    # driver.get(URL)
+    # driver.find_element_by_class_name("js-bundle-dropdown").click()
+    # div: WebElement = driver.find_element_by_class_name("bundle-dropdown-content-wrapper")
+    # elements: List[WebElement] = div.find_elements_by_css_selector("span.name")
+    elements = Scraper().scrape_bundles()
     print([ele.text for ele in elements])
 
 
--- core/scraper.py
+++ core/scraper.py
@@ -1,33 +1,44 @@
-from typing import Any, Dict, List, Generator
+from typing import Any, Dict, Generator, List
 
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webelement import WebElement
 
 TEST_URL: str = "https://www.humblebundle.com/books/hacking-101-no-starch-press-books?hmb_source=humble_home&hmb_medium=product_tile&hmb_campaign=mosaic_section_2_layout_index_2_layout_type_twos_tile_index_1_c_hacking101nostarchpress_bookbundle"
 BUNDLE_URL: str = "https://www.humblebundle.com/store"
 
 
 class Scraper:
-    BOOK_TITLE_SEARCH: Dict[str, Any] = {
+    _BOOK_TITLE_SEARCH: Dict[str, Any] = {
         "name": "span",
         "attrs": {
             "class": "front-page-art-image-text"
         }
     }
 
-    BUNDLE_TITLE_SEARCH: Dict[str, Any] = {
+    _BUNDLE_TITLE_SEARCH: Dict[str, Any] = {
         "name": "span",
-        "attrs": {
-            "class": "name"
-        }
+        "attrs": {"class": "name"}
     }
 
-    def __init__(self, url: str, search_element: Dict[str, Any] = BOOK_TITLE_SEARCH) -> None:
-        super().__init__()
-        self.url = url
-        self.search_element = search_element
+    # Initialize the selenium stuff
+    _CHROME_OPTIONS = Options()
+    _CHROME_OPTIONS.add_argument("--headless")
+    _CHROME_OPTIONS.add_argument("--window-size=1920,1080")
+    _CHROME_OPTIONS.binary_location = "/usr/bin/google-chrome"
+    _EXECUTABLE_PATH = "lib/chromedriver"
+    _HOME_PAGE = "https://www.humblebundle.com/"
+    _DROP_DOWN_BUTTON_SELECTOR = ".js-bundle-dropdown"
+    _BUNDLE_TITLE_SELECTOR = "span.name"
 
-    def scrape(self) -> Generator[str, None, List[str]]:
+    def __init__(self) -> None:
+        super().__init__()
+        self.driver = Scraper.__init_web_driver()
+
+    def scrape_book(self, url: str) -> Generator[str, None, List[str]]:
         ls: List = list()
         for span in BeautifulSoup(self.__get_book_html(),
                                   "html.parser").find_all(**self.search_element):
@@ -36,7 +47,21 @@ class Scraper:
             yield txt
         return ls
 
-    def __get_book_html(self):
+    def scrape_bundles(self) -> List[WebElement]:
+        self.driver.get(Scraper._HOME_PAGE)
+        self.driver.find_element_by_css_selector(Scraper._DROP_DOWN_BUTTON_SELECTOR).click()
+        # div: WebElement = self.driver.find_element_by_class_name("bundle-dropdown-content-wrapper")
+        elements: List[WebElement] = self.driver.find_elements_by_css_selector(
+                Scraper._BUNDLE_TITLE_SELECTOR)
+        return elements
+
+    @staticmethod
+    def __init_web_driver() -> WebDriver:
+        return webdriver.Chrome(
+                executable_path=Scraper._EXECUTABLE_PATH,
+                options=Scraper._CHROME_OPTIONS)
+
+    def __get_book_html(self) -> str:
         return requests.get(self.url).text
 
     def __repr__(self) -> Dict[str, Any]:
