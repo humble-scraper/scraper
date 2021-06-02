8c55d6c3518fd6d3c10f3ff4fd4879ff4e9270be
JustinLohKingWei <justinlohkingwei@gmail.com>
2021-06-01 20:37:56 -0400

    weird debug push

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 import { urlFor } from "../util";
 
+const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  align-items: center;
+`;
+
 const ImgLogo = styled.img.attrs({
   src: urlFor("logo.png"),
 })`
@@ -9,24 +15,29 @@ const ImgLogo = styled.img.attrs({
   box-sizing: initial;
 `;
 
-const Search = styled.input.attrs({
-})`
-   width: 100%;
+const Search = styled.input.attrs({})`
+  width: 100%;
   height: 1%;
   padding: 1.25em;
   box-sizing: initial;
   margin: 1em;
 `;
-const SearchBar = styled.div`
 
-
+const SearchBarButton = styled.button.attrs({
+  children: "Go!",
+})`
+  background-color: red;
+  height: 60%;
 `;
 
-const Row = styled.div`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  align-items: top;
-`;
+const SearchBar = styled.div.attrs({
+  children: (
+    <Row>
+      <Search />
+      <SearchBarButton />
+    </Row>
+  ),
+})``;
 
 const Navbar = styled.div.attrs({
   children: (
@@ -36,16 +47,18 @@ const Navbar = styled.div.attrs({
     </Row>
   ),
 })`
-  background-color: #494f5c;
-  color: #d8d8da;
-  border-bottom: 1px #373c46 solid;
-  box-shadow: 1px 2px 3px #00000045;
-  height: 65px;
-  width: 100%;
-  display: "flex";
-  justify-content: center;
-  align-items: center;
-  position: sticky;
+  &&& {
+    background-color: #494f5c !important;
+    color: #d8d8da;
+    border-bottom: 1px #373c46 solid;
+    box-shadow: 1px 2px 3px #00000045;
+    height: 5em;
+    width: 100%;
+    display: "flex";
+    justify-content: center;
+    align-items: center;
+    position: sticky;
+  }
 `;
 
 export default Navbar;
