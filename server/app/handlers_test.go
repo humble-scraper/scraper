d70a87c1cef3287ecb12a7b6ed0d382928b7ca2a
Ethan <ebenabou@ineat.ca>
2021-06-08 17:56:28 -0400

    Begun the refactor

---
+++ server/app/handlers_test.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Tests: GET /healthcheck
+func TestHealthcheck(t *testing.T) {
+	assert.True(t, true, "TODO: implement a unit test")
+}
